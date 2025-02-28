831b75012ecb4cece64e1dd3dceb65432176a63d
androidZ200 <androidZ200daniilsamara@mail.ru>
2025-02-28 13:36:47 +0400

    standart input for cat_vs_dog dataset

--- datasets/catvsdog/cvd_dataset.m
+++ datasets/catvsdog/cvd_dataset.m
@@ -1,9 +1,38 @@
 
 load('datasets/catvsdog/CvD.mat');
 
+if ~exist('Width_image', 'var'); Width_image = N(1,1); end
+if ~exist('Height_image', 'var'); Height_image = N(1,2); end
+
 % Labels numbers
 Labels = {'cat', 'dog'};
 ln = length(Labels);
+
+% Standart size images
+for iter = 1:length(cat)
+    scx = size(cat{iter},2)/Width_image;
+    scy = size(cat{iter},1)/Height_image;
+    sc = max(scx, scy);
+    imag = im2gray(cat{iter});
+    if sc > 1
+        imag = imresize(imag,1/sc);
+    end
+    cat{iter} = zeros(Width_image, Height_image, 'single');
+    cat{iter}(floor(end/2-size(imag,1)/2)+1:floor(end/2+size(imag,1)/2),...
+              floor(end/2-size(imag,2)/2)+1:floor(end/2+size(imag,2)/2)) = imag;
+end
+for iter = 1:length(dog)
+    scx = size(dog{iter},2)/Width_image;
+    scy = size(dog{iter},1)/Height_image;
+    sc = max(scx, scy);
+    imag = im2gray(dog{iter});
+    if sc > 1
+        imag = imresize(imag,1/sc);
+    end
+    dog{iter} = zeros(Width_image, Height_image, 'single');
+    dog{iter}(floor(end/2-size(imag,1)/2)+1:floor(end/2+size(imag,1)/2),...
+              floor(end/2-size(imag,2)/2)+1:floor(end/2+size(imag,2)/2)) = imag;
+end
 
 % Create test set
 Testsize = 2000;
@@ -14,9 +43,13 @@ Test = [cat(catid), dog(dogid)];
 TestLabel = [repmat(1, [1 Testsize]), repmat(2, [1 Testsize])];
 cat(catid) = []; dog(dogid) = [];
 
+Test = cell2mat(reshape(Test,1,1,[]));
+
 % Create training set
 Train = [cat, dog];
 TrainLabel = [repmat(1, [1 length(cat)]), repmat(2, [1 length(dog)])];
+
+Train = cell2mat(reshape(Train,1,1,[]));
 
 % coordinates of the centers of the focus areas
 if exist('coords', 'var') ~= 1
@@ -26,4 +59,4 @@ end
 MASK = single((abs(X{end} - permute(coords(:,1), [3 2 1])) < G_size_x/2).*...
               (abs(Y{end} - permute(coords(:,2), [3 2 1])) < G_size_y/2));
 
-clearvars Testsize catid dogid cat dog;
+clearvars Testsize catid dogid cat dog imag scx scy sc iter;
