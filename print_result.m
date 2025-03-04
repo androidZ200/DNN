d0736d21b43fc14f8e1b4325d94a48528371eb5a
androidZ200 <androidZ200daniilsamara@mail.ru>
2025-03-04 12:40:04 +0400

    fix print result script

--- print_result.m
+++ print_result.m
@@ -1,6 +1,6 @@
 
 if ~exist('Size', 'var'); Size = 6; end
-if ~exist('delay', 'var'); delay = 0.2; end
+if ~exist('delay', 'var'); delay = 0.5; end
 
 % colormap in shades of Samara university
 ssau = [linspace(1,  32/255, 50), linspace( 32/255, 0, 100); ...
@@ -9,8 +9,7 @@ ssau = [linspace(1,  32/255, 50), linspace( 32/255, 0, 100); ...
 % to draw squares
 xx = [-1 -1 1 1 -1]*G_size_x/2;
 yy = [1 -1 -1 1 1]*G_size_y/2;
-F = zeros(N,N,length(Propagations)+1);
-GPU_CPU;
+F = create_cells(N,'zeros',is_gpu);
        
 fig = figure('position', [100 100 1500 400]);
 for num=1:ln
@@ -20,22 +19,21 @@ for num=1:ln
         nt = randi([1,length(ind)]);
         W = Test(:,:,ind(nt));
         
-        F(:,:,1) = GetImage(W);
-        for iter11=1:size(F,3)-1
-            F(:,:,iter11+1) = Propagations{iter11}(F(:,:,iter11).*DOES(:,:,iter11));
+        F{1} = GetImage(W);
+        for iter4=1:length(DOES)
+            F{iter4+1} = FPropagations{iter4}(F{iter4}.*DOES{iter4});
         end
-        tmp = get_scores(F(:,:,end),MASK,is_max);
+        tmp = get_scores(F{end},MASK,is_max);
         tmp = tmp(1:ln);
         tmp = tmp./sum(tmp);
 
         % drawing images
-        colormap(ssau);
-		subplot(1, 3, 1);
-		imagesc(abs(W));
+		ax = subplot(1, 3, 1);
+		imagesc(abs(W)); colormap(ax, gray);
         set(gca,'xtick',[],'ytick',[]);
         axis square;
-		subplot(1, 3, 2);
-		imagesc([-B B], [-B B], abs(F(:,:,end)).^2);
+		ax = subplot(1, 3, 2);
+		imagesc([X{end}(1) X{end}(end)], [Y{end}(1) Y{end}(end)], abs(F{end}).^2); colormap(ax, ssau);
         C = max(max(abs(coords)));
         xlim([-C C]*1.5); ylim([-C C]*1.5);
         set(gca,'xtick',[],'ytick',[]);
@@ -63,4 +61,4 @@ for num=1:ln
 end
 close(fig);
 
-clearvars fig nt iter10 iter11 Size num F W tmp ssau xx yy ind C delay;
+clearvars fig nt iter10 iter11 Size num F W tmp ssau xx yy ind C delay ax;
