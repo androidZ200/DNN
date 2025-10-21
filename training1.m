4a33e6eeeb8b2a884659f95579513c1a224543cc
androidZ200 <androidZ200daniilsamara@mail.ru>
2025-10-21 12:22:57 +0400

    exlude epoch in training1

--- training1.m
+++ training1.m
@@ -42,120 +42,116 @@ tt1 = tic;
 tt_backup = tic;
 last_backup_time = toc(tt_backup);
 if disp_info >= 1; ndisp(); end
-if ~exist('ep', 'var'); ep = 1; end
-for ep=ep:epoch
-    if ~exist('randind', 'var')
-        randind = randperm(size(Train,3));
-        randind = randind(1:P);
-    end
-    if ~exist('iter7', 'var'); iter7 = 1-batch; end
-    for iter7=iter7+batch:batch:P
-        gradient = zero_grad;
+randind = [];
+for iter8=1:epoch
+    randind = [randind randperm(size(Train,3), P)];
+end
+if ~exist('iter7', 'var'); iter7 = 1-batch; end
+for iter7=iter7+batch:batch:length(randind)
+    gradient = zero_grad;
 
-        % rand offsets
-        if max_offsets > 0
-            off = randi(3, length(DOES), 2)-2;
-            off = off*max_offsets;
-            for iter8 = 1:size(DOES,3)
-                DOES{iter8} = circshift(DOES{iter8}, off(iter8,:));
-            end
-        end
-        
-        for iter9=0:max_batch:(batch-1)
-            index = randind(iter7+iter9+(0:min(max_batch-1, P-iter7-iter9)));
-            num = reshape(TrainLabel(index),1,[]);
-            
-            % direct propagation
-            W{1} = GetImage(Train(:,:,index));
-            for iter8=1:length(W)-1
-                W{iter8+1} = FPropagations{iter8}(W{iter8}.*DOES{iter8});
-            end
-            [me, mi] = get_scores(permute(W{end},[1 2 4 3]), MASK, is_max);
-            I = sum(me);
-            me = me./I;
-            Accr = Accr + sum(max(me) == me(num+(0:min(max_batch-1, P-iter7-iter9))*size(MASK,3)));
-            cAccr = cAccr + min(max_batch, P-iter7-iter9+1);
-
-            % training
-            Wend = conj(W{end});
-            switch LossFunc
-                case 'Sosh'
-                    p = me >= me(num+(0:min(max_batch-1, P-iter7-iter9))*size(MASK,3));
-                    p = -(sum(me.*p)./sum(p) - me).*p;
-                    d = sqrt(sum(p.^2));
-                    p = p./d.*exp(-d*sosh_factor); p(isnan(p)) = 0;
-                    p = 2*(p-sum(me.*p))./I;
-                case 'MSE' % mean squared error
-                    p = me - target_scores(:,num);
-                    p = 4*(p-sum(me.*p))./I;
-                case 'MAE' % mean absolute error
-                    p = me - target_scores(:,num);
-                    p = p ./ abs(p); p(isnan(p)) = 0;
-                    p = 2*(p-sum(me.*p))./I;
-                case 'SCE' % softmax cross entropy
-                    p = exp(sce_factor*me); 
-                    p = p./sum(p);
-                    alpha = target_scores(:,num);
-                    p = (p-sum(p.*me)).*sum(alpha) + sum(alpha.*me) - alpha;
-                    p = p*sce_factor*2./I;
-                otherwise
-                    error(['Loss function "' name '" is not exist']);
-            end
-            F{end} = Wend.*permute(sum(permute(p,[3 4 1 2]).*mi,3),[1 2 4 3]);
-            if joint_factor > 0
-                F{end} = (1-joint_factor)*F{end} - 2*joint_factor./permute(I, [1 3 2]).*Wend.*sum(MASK,3);
-            end
-
-            % reverse propagation
-            for iter8=length(F)-1:-1:deep_grad
-                F{iter8} = BPropagations{iter8}(F{iter8+1}).*DOES{iter8};
-            end
-            gradient = cellfun(@(gr,w,f)gr-imag(sum(w.*f,3)), gradient,W(1:end-1),F(1:end-1),'UniformOutput',false);
-
-            if disp_info >= 1
-                rdisp(['iter = ' num2str(iter7+iter9+max_batch-1 + (ep-1)*P) '/' num2str(P*epoch) '; accr = ' ...
-                    num2str(Accr/cAccr*100) ...
-                    '%; time = ' num2str(toc(tt1)) ';']);
-            end
-        end
-
-        % reverse offsets
-        if max_offsets > 0
-            for iter8 = 1:length(DOES)
-                DOES{iter8} = circshift(DOES{iter8}, -off(iter8,:));
-                gradient{iter8} = circshift(gradient{iter8}, -off(iter8,:));
-            end
-        end
-
-        % updating weights
-        iter_gradient = iter_gradient + 1;
-        [gradient, tmp_data] = criteria(gradient, tmp_data, method, [params, iter_gradient]);
-        DOES = cellfun(@(DOES,gradient,GRAD_MASK)DOES.*exp(-1i*speed*gradient.*GRAD_MASK), ...
-            DOES,gradient,GRAD_MASK,'UniformOutput',false);
-        speed = speed*slowdown;
-        
-        % backup
-        if is_backup && toc(tt_backup) - last_backup_time > backup_time
-            if disp_info >= 2; rdisp('backuping...'); end
-            save('training_backup');
-            last_backup_time = toc(tt_backup);
-        end
-
-        % data output to the console
-        if mod(iter7+batch-1 + (ep-1)*P, cycle) == 0
-            Accr = Accr/cAccr*100;
-            accr_graph(end+1) = Accr;
-            Accr = 0; cAccr = 0;
-            ndisp();
+    % rand offsets
+    if max_offsets > 0
+        off = randi(3, length(DOES), 2)-2;
+        off = off*max_offsets;
+        for iter8 = 1:size(DOES,3)
+            DOES{iter8} = circshift(DOES{iter8}, off(iter8,:));
         end
     end
-    clearvars iter7 randind;
+    
+    for iter9=0:max_batch:(batch-1)
+        index = randind(iter7+iter9+(0:min(max_batch-1, length(randind)-iter7-iter9)));
+        num = reshape(TrainLabel(index),1,[]);
+        
+        % direct propagation
+        W{1} = GetImage(Train(:,:,index));
+        for iter8=1:length(W)-1
+            W{iter8+1} = FPropagations{iter8}(W{iter8}.*DOES{iter8});
+        end
+        [me, mi] = get_scores(permute(W{end},[1 2 4 3]), MASK, is_max);
+        I = sum(me);
+        me = me./I;
+        Accr = Accr + sum(max(me) == me(num+(0:min(max_batch-1, length(randind)-iter7-iter9))*size(MASK,3)));
+        cAccr = cAccr + min(max_batch, length(randind)-iter7-iter9+1);
+
+        % training
+        Wend = conj(W{end});
+        switch LossFunc
+            case 'Sosh'
+                p = me >= me(num+(0:min(max_batch-1, length(randind)-iter7-iter9))*size(MASK,3));
+                p = -(sum(me.*p)./sum(p) - me).*p;
+                d = sqrt(sum(p.^2));
+                p = p./d.*exp(-d*sosh_factor); p(isnan(p)) = 0;
+                p = 2*(p-sum(me.*p))./I;
+            case 'MSE' % mean squared error
+                p = me - target_scores(:,num);
+                p = 4*(p-sum(me.*p))./I;
+            case 'MAE' % mean absolute error
+                p = me - target_scores(:,num);
+                p = p ./ abs(p); p(isnan(p)) = 0;
+                p = 2*(p-sum(me.*p))./I;
+            case 'SCE' % softmax cross entropy
+                p = exp(sce_factor*me); 
+                p = p./sum(p);
+                alpha = target_scores(:,num);
+                p = (p-sum(p.*me)).*sum(alpha) + sum(alpha.*me) - alpha;
+                p = p*sce_factor*2./I;
+            otherwise
+                error(['Loss function "' name '" is not exist']);
+        end
+        F{end} = Wend.*permute(sum(permute(p,[3 4 1 2]).*mi,3),[1 2 4 3]);
+        if joint_factor > 0
+            F{end} = (1-joint_factor)*F{end} - 2*joint_factor./permute(I, [1 3 2]).*Wend.*sum(MASK,3);
+        end
+
+        % reverse propagation
+        for iter8=length(F)-1:-1:deep_grad
+            F{iter8} = BPropagations{iter8}(F{iter8+1}).*DOES{iter8};
+        end
+        gradient = cellfun(@(gr,w,f)gr-imag(sum(w.*f,3)), gradient,W(1:end-1),F(1:end-1),'UniformOutput',false);
+
+        if disp_info >= 1
+            rdisp(['iter = ' num2str(iter7+iter9+max_batch-1) '/' num2str(length(randind)) '; accr = ' ...
+                num2str(Accr/cAccr*100) ...
+                '%; time = ' num2str(toc(tt1)) ';']);
+        end
+    end
+
+    % reverse offsets
+    if max_offsets > 0
+        for iter8 = 1:length(DOES)
+            DOES{iter8} = circshift(DOES{iter8}, -off(iter8,:));
+            gradient{iter8} = circshift(gradient{iter8}, -off(iter8,:));
+        end
+    end
+
+    % updating weights
+    iter_gradient = iter_gradient + 1;
+    [gradient, tmp_data] = criteria(gradient, tmp_data, method, [params, iter_gradient]);
+    DOES = cellfun(@(DOES,gradient,GRAD_MASK)DOES.*exp(-1i*speed*gradient.*GRAD_MASK), ...
+        DOES,gradient,GRAD_MASK,'UniformOutput',false);
+    speed = speed*slowdown;
+    
+    % backup
+    if is_backup && toc(tt_backup) - last_backup_time > backup_time
+        if disp_info >= 2; rdisp('backuping...'); end
+        save('training_backup');
+        last_backup_time = toc(tt_backup);
+    end
+
+    % data output to the console
+    if mod(iter7+batch-1, cycle) == 0
+        Accr = Accr/cAccr*100;
+        accr_graph(end+1) = Accr;
+        Accr = 0; cAccr = 0;
+        ndisp();
+    end
 end
 if disp_info >= 2; ndisp('training1 finished'); end
 
 %% clearing unnecessary variables
 
-clearvars num iter7 iter8 iter9 ep randind me mi W Wend F Accr cAccr gradient p I alpha tt1 ...
+clearvars num iter7 iter8 iter9 randind me mi W Wend F Accr cAccr gradient p I alpha tt1 ...
     d tt_backup last_backup_time index zero_grad deep_grad;
 if deleted == true
     clearvars P epoch speed slowdown batch LossFunc method params cycle deleted tmp_data ...
