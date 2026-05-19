8b62bfa5c229d4209ac8b42d863e91461a305a62
androidZ200 <androidZ200daniilsamara@mail.ru>
2026-05-19 11:02:36 +0400

    update training1.m and check_result.m

--- check_result.m
+++ check_result.m
@@ -1,57 +1,42 @@
 
-TestScores = zeros(size(MASK,3), size(Test,3), 'single'); % scores
-TestLabel = reshape(TestLabel, [], 1);
+TestScores = zeros(OptSystem.Output.count_outputs(), size(Test,3), 'single'); % scores
 
 if ~exist('max_batch', 'var'); max_batch = 40; end
-W = create_cells(N,max_batch,'zeros',is_gpu);
 max_batch = min(size(Test,3), max_batch);
-total_enegry = 0;
-additionalinfo = '';
 
 ttcr = tic();
-if disp_info >= 1; ndisp(['check result\n' waitbartext(60, -1) ' 0%']); end
+ndisp(['check result\n' waitbartext(60, -1) ' 0%']);
 for iter3=1:size(Test,3)/max_batch
     % running through the system
-    W{1} = GetImage(Test(:,:,(iter3-1)*max_batch+1:iter3*max_batch));
-    for iter4=1:length(DOES)
-        W{iter4+1} = FPropagations{iter4}(DOES{iter4}.*W{iter4});
-    end
-    total_enegry = total_enegry + sum(abs(W{end}).^2.*sum(MASK,3), 'all');
-    TestScores(:,(iter3-1)*max_batch+1:iter3*max_batch) = get_scores(permute(W{end},[1 2 4 3]), MASK, is_max);
+    TestScores(:,(iter3-1)*max_batch+1:iter3*max_batch) = ...
+        OptSystem.Forward(Test(:,:,(iter3-1)*max_batch+1:iter3*max_batch));
     
-    if disp_info >= 2
-        [~, argmax] = max(TestScores(:,1:iter3*max_batch));
-        additionalinfo = ['\nwaiting time ' num2str(toc(ttcr)*(size(Test,3)/max_batch/iter3 - 1), '%.2f') ...
-            's\ncurrent accuracy ' num2str(mean(argmax' == TestLabel(1:iter3*max_batch))*100, '%.2f') '%'];
-    end
-    if disp_info >= 1; rdisp(['check result\n' waitbartext(60, iter3/size(Test,3)*max_batch) ...
-            ' ' num2str(iter3*max_batch/size(Test,3)*100,'%.2f') '%' additionalinfo]); end
+    rdisp(['check result\n' waitbartext(60, iter3/size(Test,3)*max_batch) ...
+            ' ' num2str(iter3*max_batch/size(Test,3)*100,'%.2f') '%']);
 end
-if disp_info >= 1; rdisp(['check result takes time: ' num2str(toc(ttcr)) 's']); end
+rdisp(['check result takes time: ' num2str(toc(ttcr)) 's']);
 %%
 % error table
 [~, argmax] = max(TestScores);
-err_tabl = zeros(size(MASK,3), ln, size(Test,3), 'single');
-err_tabl(argmax + size(MASK,3)*(reshape(TestLabel,1,[])-1) + size(MASK,3)*ln*(0:(size(Test,3)-1))) = 1;
+err_tabl = zeros(OptSystem.Output.count_outputs(), length(Labels), size(Test,3), 'single');
+err_tabl(argmax + OptSystem.Output.count_outputs()*(reshape(TestLabel,1,[])-1) + ...
+    OptSystem.Output.count_outputs()*length(Labels)*(0:(size(Test,3)-1))) = 1;
 err_tabl = sum(err_tabl,3);
 
 % intensity table
-onehot = reshape(TestLabel,1,[]) == (1:ln)';
+onehot = reshape(TestLabel,1,[]) == (1:length(Labels))';
 int_tabl = (TestScores./sum(TestScores)) * onehot';
 %%
 % accuracy info
 accuracy = sum(diag(err_tabl))/sum(sum(err_tabl))*100;
 int_tabl = int_tabl./sum(int_tabl)*100;
-if disp_info >= 1; ndisp(['accuracy = ' num2str(accuracy) '%;']); end
+ndisp(['accuracy = ' num2str(accuracy) '%;']);
 % min contrast info
 T = sort(int_tabl);
 min_contrast = min((T(end,:) - T(end-1,:))./(T(end,:) + T(end-1,:))*100);
-if disp_info >= 1; ndisp(['min contrast = ' num2str(min_contrast) '%;']); end
-% effectiveness info
-total_enegry = total_enegry/size(Test,3);
-if disp_info >= 1; ndisp(['total energy = ' num2str(total_enegry*100) '%']); end
+ndisp(['min contrast = ' num2str(min_contrast) '%;']);
 
-clearvars argmax W iter3 iter4 num T max_batch onehot ttcr;
+clearvars max_batch ttcr iter3 argmax onehot T;
 return
 
 
@@ -61,11 +46,11 @@ grad = 100;
 figure;
 imagesc(bsxfun(@rdivide, err_tabl, sum(err_tabl)));
 xlabel('input class'); ylabel('recognized class');
-xticks(1:ln); yticks(1:size(MASK,3));
+xticks(1:size(err_tabl,1)); yticks(1:size(err_tabl,2));
 xticklabels(Labels); yticklabels(Labels);
 colormap([linspace(1, 32/255, grad)', linspace(1, 145/255, grad)', linspace(1, 201/255, grad)']);
-for ii = 1:ln
-    for jj = 1:size(MASK,3)
+for ii = 1:size(err_tabl,2)
+    for jj = 1:size(err_tabl,1)
         color = [0 0 0];
         if err_tabl(jj, ii)/sum(err_tabl(:, ii)) > 0.5
             color = [1 1 1];
@@ -86,11 +71,11 @@ grad = 100;
 figure;
 imagesc(int_tabl);
 xlabel('input class'); ylabel('recognized class');
-xticks(1:ln); yticks(1:size(MASK,3));
+xticks(1:size(int_tabl,2)); yticks(1:size(int_tabl,1));
 xticklabels(Labels); yticklabels(Labels);
 colormap([linspace(1, 201/255, grad)', linspace(1, 88/255, grad)', linspace(1, 32/255, grad)']);
-for ii = 1:ln
-    for jj = 1:size(MASK,3)
+for ii = 1:size(int_tabl,2)
+    for jj = 1:size(int_tabl,1)
         color = [0 0 0];
         text(ii, jj, replace(sprintf('%.1f', int_tabl(jj, ii)), '.', ','), 'fontsize', 14, ...
             'color', color, 'HorizontalAlignment', 'center');
--- training1.m
+++ training1.m
@@ -1,156 +1,81 @@
 % gradient learning method
 
-if ~exist('P', 'var'); P = size(Train,3); end
+if ~exist('Train', 'var'); error('Train database has not loaded'); end
+if ~exist('TrainLabel', 'var'); error('TrainLabel has not loaded'); end
+if ~exist('OptSystem', 'var'); error('OptSystem has not created'); end
+
 if ~exist('epoch', 'var'); epoch = 1; end
-if ~exist('speed', 'var'); speed = 1e-1; end
-if ~exist('slowdown', 'var'); slowdown = 0.999; end
-if ~exist('batch', 'var'); batch = 20; end
+if ~exist('speed', 'var'); error('speed is not define'); end
+if ~exist('slowdown', 'var'); slowdown = 1; end
+if ~exist('batch', 'var'); error('batch is not define'); end
 if ~exist('max_batch', 'var'); max_batch = batch; end
-if ~exist('LossFunc', 'var'); LossFunc = 'SCE'; end
-if ~exist('cycle', 'var'); cycle = 200; end
-if ~exist('deleted', 'var'); deleted = true; end
-if ~exist('sce_factor', 'var') && strcmp(LossFunc, 'SCE'); sce_factor = 80; end
-if ~exist('sosh_factor', 'var') && strcmp(LossFunc, 'Sosh'); sosh_factor = 10; end
-if ~exist('joint_factor', 'var'); joint_factor = 0; end
-if ~exist('target_scores', 'var'); target_scores = eye(size(MASK,3),ln,'single'); end
-if ~exist('max_offsets', 'var'); max_offsets = 0; end
-if ~exist('optimizer', 'var'); optimizer = SGD_optimizer(); end
+if ~exist('LossFunc', 'var'); error('LossFunc is not define'); end
+if ~exist('target', 'var'); target = eye(OptSystem.Output.count_outputs(),length(unique(TrainLabel))); end
+if ~exist('cycle', 'var'); cycle = size(Train,3); end
+
 if ~exist('is_backup', 'var'); is_backup = false; end
 if ~exist('backup_time', 'var') && is_backup; backup_time = 3600; end
-if ~exist('Accr', 'var'); Accr = 0; end
-if ~exist('cAccr', 'var'); cAccr = 0; end
 
-if disp_info >= 2; ndisp('start training1'); end
-batch = min(batch, P);
-accr_graph(1) = nan;
+batch = min(batch, size(Train,3));
 max_batch = min(batch, max_batch);
-
-zero_grad = create_cells(N(1:end-1,:),1,'zeros',is_gpu);
-W = create_cells(N,max_batch,'zeros',is_gpu);
-F = create_cells(N,max_batch,'zeros',is_gpu);
-
-for iter=1:length(F)
-    deep_grad = iter;
-    if sum(sum(DOES{iter}.mask)) > 0; break; end
-end
+target = GPUTest(target);
 
 %% training
 tt1 = tic;
 tt_backup = tic;
 last_backup_time = toc(tt_backup);
-if disp_info >= 1; ndisp(); end
+loss = 0; accr = 0;
+ndisp();
+
 randind = [];
 for iter8=1:epoch
-    randind = [randind randperm(size(Train,3), P)];
+    randind = [randind randperm(size(Train,3), size(Train,3))];
 end
 if ~exist('iter7', 'var'); iter7 = 1-batch; end
-for iter7=iter7+batch:batch:length(randind)
-    gradient = zero_grad;
-
-    % rand offsets
-    if max_offsets > 0
-        off = randi(3, length(DOES), 2)-2;
-        off = off*max_offsets;
-        for iter8 = 1:length(DOES)
-            DOES{iter8} = circshift(DOES{iter8}, off(iter8,:));
-        end
-    end
-    
+for iter7=iter7+batch:batch:length(randind)   
+    gradient = [];
     for iter9=0:max_batch:(batch-1)
         index = randind(iter7+iter9+(0:min(max_batch-1, length(randind)-iter7-iter9)));
         num = reshape(TrainLabel(index),1,[]);
         
         % direct propagation
-        W{1} = GetImage(Train(:,:,index));
-        for iter8=1:length(W)-1
-            W{iter8+1} = FPropagations{iter8}(DOES{iter8}.*W{iter8});
-        end
-        [me, mi] = get_scores(permute(W{end},[1 2 4 3]), MASK, is_max);
-        I = sum(me);
-        me = me./I;
-        Accr = Accr + sum(max(me) == me(num+(0:min(max_batch-1, length(randind)-iter7-iter9))*size(MASK,3)));
-        cAccr = cAccr + min(max_batch, length(randind)-iter7-iter9+1);
+        score = OptSystem.Forward(Train(:,:,index));
+        [~, maxind] = max(score);
+        accr = accr*0.95 + 0.05*mean(maxind == num);
+        % get gradient
+        loss = loss*0.95 + 0.05*mean(LossFunc.get_error(score, target(:,num)));
+        error = LossFunc.get_gradient(score, target(:,num));
+        % back propagation
+        gradient = cellsum(OptSystem.Backward(error), gradient);
 
-        % training
-        Wend = conj(W{end});
-        switch LossFunc
-            case 'Sosh'
-                p = me >= me(num+(0:min(max_batch-1, length(randind)-iter7-iter9))*size(MASK,3));
-                p = -(sum(me.*p)./sum(p) - me).*p;
-                d = sqrt(sum(p.^2));
-                p = p./d.*exp(-d*sosh_factor); p(isnan(p)) = 0;
-                p = 2*(p-sum(me.*p))./I;
-            case 'MSE' % mean squared error
-                p = me - target_scores(:,num);
-                p = 4*(p-sum(me.*p))./I;
-            case 'MAE' % mean absolute error
-                p = me - target_scores(:,num);
-                p = p ./ abs(p); p(isnan(p)) = 0;
-                p = 2*(p-sum(me.*p))./I;
-            case 'SCE' % softmax cross entropy
-                p = exp(sce_factor*me); 
-                p = p./sum(p);
-                alpha = target_scores(:,num);
-                p = (p-sum(p.*me)).*sum(alpha) + sum(alpha.*me) - alpha;
-                p = p*sce_factor*2./I;
-            otherwise
-                error(['Loss function "' name '" is not exist']);
-        end
-        F{end} = Wend.*permute(sum(permute(p,[3 4 1 2]).*mi,3),[1 2 4 3]);
-        if joint_factor > 0
-            F{end} = (1-joint_factor)*F{end} - 2*joint_factor./permute(I, [1 3 2]).*Wend.*sum(MASK,3);
-        end
-
-        % reverse propagation
-        for iter8=length(F)-1:-1:deep_grad
-            F{iter8} = DOES{iter8}.*BPropagations{iter8}(F{iter8+1});
-        end
-        gradient = cellfun(@(gr,w,f)gr+sum(w.*f,3), gradient,W(1:end-1),F(1:end-1),'UniformOutput',false);
-
-        if disp_info >= 1
-            rdisp(['iter = ' num2str(iter7+iter9+max_batch-1) '/' num2str(length(randind)) '; accr = ' ...
-                num2str(Accr/cAccr*100) '%; time = ' num2str(toc(tt1)) ';']);
-        end
-    end
-
-    % reverse offsets
-    if max_offsets > 0
-        for iter8 = 1:length(DOES)
-            DOES{iter8} = circshift(DOES{iter8}, -off(iter8,:));
-            gradient{iter8} = circshift(gradient{iter8}, -off(iter8,:));
-        end
+        
+        % display info
+        progres = (iter7+iter9+max_batch-1) / length(randind);
+        first_line = ['[' num2str(progres*100,'%05.2f') '%]; loss = ' num2str(loss,'%.3e') ...
+            '; accr = ' num2str(accr*100,'%.2f') '%'];
+        rdisp([first_line '\n' waitbartext(50, progres) ' time = ' num2str(toc(tt1)) ';']);
     end
 
     % updating weights
-    gradient = cellfun(@(DOES,error) DOES.get_gradient(error),DOES,gradient,'UniformOutput',false);
-    gradient = optimizer.optimize(gradient);
-    cellfun(@(DOES,gradient)DOES.gradient_step(-speed*gradient),DOES,gradient,'UniformOutput',false);
+    OptSystem.Step(gradient,speed);
     speed = speed*slowdown;
     
     % backup
     if is_backup && toc(tt_backup) - last_backup_time > backup_time
-        if disp_info >= 2; rdisp('backuping...'); end
+        rdisp('backuping...');
         save('training_backup');
         last_backup_time = toc(tt_backup);
     end
 
     % data output to the console
     if mod(iter7+batch-1, cycle) == 0
-        Accr = Accr/cAccr*100;
-        accr_graph(end+1) = Accr;
-        Accr = 0; cAccr = 0;
+        rdisp(first_line);
         ndisp();
     end
 end
-if disp_info >= 2; ndisp('training1 finished'); end
 
 %% clearing unnecessary variables
 
-clearvars num iter7 iter8 iter9 randind me mi W Wend F Accr cAccr gradient p I alpha tt1 ...
-    d tt_backup last_backup_time index zero_grad deep_grad;
-if deleted == true
-    clearvars P epoch speed slowdown batch LossFunc optimizer cycle deleted ...
-        sce_factor joint_factor target_scores max_offsets sosh_factor is_backup backup_time;
-else
-    deleted = true;
-end
+clearvars epoch speed slowdown batch max_batch LossFunc target cycle is_backup backup_time tt1 ...
+    tt_backup last_backup_time loss accr randind iter8 iter7 gradient iter9 index num score ...
+    maxind error progres first_line;
